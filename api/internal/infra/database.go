cd19d0deca5a7a64a81e7507718381c0e27cae60
hrn-tmy <mj0829mj0625@icloud.com>
2026-03-20 21:31:21 +0900

    add: DB接続情報作成

---
+++ api/internal/infra/database.go
@@ -0,0 +1,40 @@
+package infra
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DBConfig struct {
+	user     string
+	pass     string
+	host     string
+	port     string
+	name     string
+	sslmode  string
+	timezone string
+}
+
+func NewDB() (*gorm.DB, error) {
+	godotenv.Load()
+	cfg := DBConfig{
+		user:     os.Getenv("DB_USER"),
+		pass:     os.Getenv("DB_PASS"),
+		host:     os.Getenv("DB_HOST"),
+		port:     os.Getenv("DB_PORT"),
+		name:     os.Getenv("DB_NAME"),
+		sslmode:  os.Getenv("SSL_MODE"),
+		timezone: os.Getenv("DB_TIMEZONE"),
+	}
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", cfg.host, cfg.user, cfg.pass, cfg.name, cfg.port, cfg.sslmode, cfg.timezone)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
